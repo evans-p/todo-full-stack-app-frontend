55477e7ca3a379672ab68c974e9517cc1b4d075f
evans-p <evanspoulaki@gmail.com>
2024-02-09 23:54:03 +0200

    Removed redundant files. Added initial config for tailwind.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-amber-800'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-yellow-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
