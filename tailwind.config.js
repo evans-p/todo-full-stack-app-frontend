1cd7a90c09d85ff4bbf79e629449d6fd4d8910ff
evans-p <evanspoulaki@gmail.com>
2024-03-07 00:55:15 +0200

    tailwind init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: "jit",
+  purge: ["./pages/**"], //TODO : add more paths to the array if needed
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
