823fd3ed2e9b095fe69eb3ebfd36eebba16314f5
evans-p <evanspoulaki@gmail.com>
2024-03-07 04:08:40 +0200

    (WIP) Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,24 @@
+import { FaGlobe } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
+
 const Nav = () => {
-  return <nav className="fixed top-0 left-0 w-screen h-14 bg-red-700"></nav>;
+  return (
+    <nav className="fixed top-0 left-0 w-screen h-16 flex justify-between items-center">
+      <section>Logo</section>
+      <section className="flex justify-between items-center">
+        <span className="flex ml-2 mr-2">
+          <FaGlobe className="text-gray-800 text-2xl cursor-pointer" />
+          <select>
+            <option>123</option>
+            <option>123</option>
+            <option>123</option>
+          </select>
+        </span>
+        <FaGlobe className="text-gray-800 text-2xl mr-2 cursor-pointer" />
+        <FaRegUserCircle className="text-gray-800 text-4xl mr-2 cursor-pointer" />
+      </section>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ const Home = () => {
     <div className="h-screen w-screen bg-gradient-image bg-contain bg-no-repeat bg-bottom flex justify-center items-center">
       <section className="isolate w-3/6 h-96 bg-white bg-opacity-40 backdrop-blur-xl rounded ring-2 ring-black/5 flex  justify-around items-center">
         <section className="flex flex-col items-start justify-start">
-          <h1 className="text-6xl font-bold mb-4">TaskForge</h1>
-          <h2 className="text-xl">
+          <h1 className="text-6xl text-gray-800 font-bold mb-4">TaskForge</h1>
+          <h2 className="text-xl text-gray-800">
             Task Management, <span className="font-medium">enganced.</span>
           </h2>
         </section>
