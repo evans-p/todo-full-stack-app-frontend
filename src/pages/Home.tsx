9b9e51d8634fd1360ffce073ef04d185859722f1
evans-p <evanspoulaki@gmail.com>
2024-03-07 03:03:34 +0200

    Resizing modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import list from "../assets/images/list.png";
 const Home = () => {
   return (
     <div className="h-screen w-screen bg-gradient-image bg-contain bg-no-repeat bg-bottom flex justify-center items-center">
-      <section className="isolate w-4/6 h-96 bg-white bg-opacity-40 backdrop-blur-xl rounded ring-2 ring-black/5 flex  justify-around items-center">
+      <section className="isolate w-3/6 h-96 bg-white bg-opacity-40 backdrop-blur-xl rounded ring-2 ring-black/5 flex  justify-around items-center">
         <section className="flex flex-col items-start justify-start">
           <h1 className="text-5xl font-bold mb-4">TaskForge</h1>
           <h2 className="text-xl">
