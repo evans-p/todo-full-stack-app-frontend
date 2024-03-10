2d213366b6f4bccb0db79b602e5027d46c298b69
evans-p <evanspoulaki@gmail.com>
2024-03-10 07:03:39 +0200

    Finalized Nav Appearance

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,23 @@
-import { FaGlobe } from "react-icons/fa6";
-import { FaRegUserCircle } from "react-icons/fa";
+import {
+  IoPersonOutline,
+  IoGlobeOutline,
+  IoMoonOutline,
+} from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
 
 const Nav = () => {
   return (
     <nav className="fixed top-0 left-0 w-screen h-16 flex justify-between items-center">
       <section>Logo</section>
       <section className="flex justify-between items-center">
-        <span className="flex ml-2 mr-2">
-          <FaGlobe className="text-gray-800 text-2xl cursor-pointer" />
-          <select>
-            <option>123</option>
-            <option>123</option>
-            <option>123</option>
-          </select>
+        <IoMoonOutline className="text-gray-800 text-2xl cursor-pointer ml-2 " />
+        <span className="flex ml-3 mr-3 items-center cursor-pointer">
+          <IoGlobeOutline className="text-gray-800 text-2xl cursor-pointer ml-2 mr-2" />
+          <p className="mr-1">EL</p>
+          <IoIosArrowDown className="text-gray-800" />
         </span>
-        <FaGlobe className="text-gray-800 text-2xl mr-2 cursor-pointer" />
-        <FaRegUserCircle className="text-gray-800 text-4xl mr-2 cursor-pointer" />
+
+        <IoPersonOutline className="text-gray-800 text-2xl mr-3 cursor-pointer" />
       </section>
     </nav>
   );
