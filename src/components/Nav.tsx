d55b4b673ba2b375fd22ca9f4ea5df7000c5647b
evans-p <evanspoulaki@gmail.com>
2024-03-12 20:41:41 +0200

    Integrated Logo to NavBar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,11 +4,12 @@ import {
   IoMoonOutline,
 } from "react-icons/io5";
 import { IoIosArrowDown } from "react-icons/io";
+import logo from "../assets/images/logo.png";
 
 const Nav = () => {
   return (
     <nav className="fixed top-0 left-0 w-screen h-16 flex justify-between items-center">
-      <section>Logo</section>
+      <img src={logo} alt="TaskForge" className="w-10 h-auto ml-3" />
       <section className="flex justify-between items-center">
         <IoMoonOutline className="text-gray-800 text-2xl cursor-pointer ml-2 " />
         <span className="flex ml-3 mr-3 items-center cursor-pointer">
