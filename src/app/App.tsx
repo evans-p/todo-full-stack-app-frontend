1dd5eeb1f1e77ec439174b0f0d5325510f9cc918
evans-p <evanspoulaki@gmail.com>
2024-03-13 01:08:59 +0200

    The commit changes the login page to the home page. It also changes the width of the home page section from 3/6 of the screen width to 600px.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import Nav from "../components/Nav";
 import Login from "../pages/Login";
+import Home from "../pages/Home";
 
 const App = () => {
   return (
     <div className="h-screen w-screen bg-gray-100">
       <Nav />
-      <Login />
+      <Home />
+      {/* <Login /> */}
     </div>
   );
 };
