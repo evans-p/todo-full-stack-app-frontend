b86a333862e66d75542b9e399969c9885993a99d
evans-p <evanspoulaki@gmail.com>
2024-03-12 20:43:09 +0200

    ninor

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,7 +9,7 @@ import logo from "../assets/images/logo.png";
 const Nav = () => {
   return (
     <nav className="fixed top-0 left-0 w-screen h-16 flex justify-between items-center">
-      <img src={logo} alt="TaskForge" className="w-10 h-auto ml-3" />
+      <img src={logo} alt="TaskForge" className="w-10 h-auto ml-8" />
       <section className="flex justify-between items-center">
         <IoMoonOutline className="text-gray-800 text-2xl cursor-pointer ml-2 " />
         <span className="flex ml-3 mr-3 items-center cursor-pointer">
@@ -17,8 +17,7 @@ const Nav = () => {
           <p className="mr-1">EL</p>
           <IoIosArrowDown className="text-gray-800" />
         </span>
-
-        <IoPersonOutline className="text-gray-800 text-2xl mr-3 cursor-pointer" />
+        <IoPersonOutline className="text-gray-800 text-2xl mr-8 cursor-pointer" />
       </section>
     </nav>
   );
