e11b157c3db4fbf07a15bf3afa0a826be41d5e69
evans-p <evanspoulaki@gmail.com>
2024-04-06 02:20:57 +0300

    "Add IoAdd icon to Main component"
    This commit adds the IoAdd icon from "react-icons/io5" to the Main component in the Main.tsx file. The icon is used in a new button element, which is positioned near the top of the component's return statement. The button also includes the text "Add List".

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,12 +9,25 @@ const Main = () => {
         <section className="w-full h-16 flex items-center justify-between">
           <img src={logo} alt="TaskForge" className="ml-6 w-10 h-auto" />
           <h1 className="text-gray-800 dark:text-gray-100">TaskForge</h1>
-          <span className=" cursor-pointer mr-6 p-2 rounded-full hover:bg-white hover:shadow focus:bg-white group dark:hover:bg-gray-600 dark:hover:shadow-gray-400 dark:hover:shadow-sm dark:focus:bg-gray-600">
+          <span className="cursor-pointer mr-6 p-2 rounded-full hover:bg-white hover:shadow focus:bg-white group dark:hover:bg-gray-600 dark:hover:shadow-gray-400 dark:hover:shadow-sm dark:focus:bg-gray-600">
             <IoMenuOutline className="text-gray-800 text-2xl  dark:text-gray-100" />
           </span>
         </section>
-        <section className="w-full h-10 flex items-center ml-6 text-gray-800 text-lg dark:text-gray-100">
-          <h2>Lists</h2>
+        <section className="px-6 py-3 text-gray-800 cursor-pointer dark:text-gray-100 hover:bg-gray-700">
+          <h2 className="text-sm mb-1">Todo List</h2>
+          <h3 className="text-xs">8 Todo(s)</h3>
+        </section>
+        <section className="w-full px-6 py-3 text-gray-800 cursor-pointer dark:text-gray-100 hover:bg-gray-700">
+          <h2 className="text-sm mb-1">Todo List</h2>
+          <h3 className="text-xs">8 Todos</h3>
+        </section>
+        <section className="px-6 py-3 text-gray-800 cursor-pointer dark:text-gray-100">
+          <h2 className="text-sm mb-1">Todo List</h2>
+          <h3 className="text-xs">8 Todos</h3>
+        </section>
+        <section className="px-6 py-3 text-gray-800 cursor-pointer dark:text-gray-100">
+          <h2 className="text-sm mb-1">Todo List</h2>
+          <h3 className="text-xs">8 Todos</h3>
         </section>
       </div>
     </div>
