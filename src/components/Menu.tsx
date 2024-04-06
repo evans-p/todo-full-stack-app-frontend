d54b0857bbbbf65f8055634195b35f06ba1a5dfb
evans-p <evanspoulaki@gmail.com>
2024-04-06 21:37:54 +0300

    "Replace hardcoded menu sections with dynamic list generation"
    This commit removes the hardcoded menu sections for the todo lists and replaces them with a dynamic list generation function. The function generates an array of section elements for the menu, which are then mapped to JSX elements and rendered. This change simplifies the code and makes it easier to add or remove todo list sections in the future.

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,29 +3,22 @@ import { IoAdd } from "react-icons/io5";
 import logo from "../assets/images/logo.png";
 
 const Menu = () => {
-  return (
-    <div className="fixed left-0 top-0 w-80 h-screen bg-gray-950 text-xl text-gray-100">
-      <section className="w-full flex items-center justify-start mt-3 mb-6">
-        <img src={logo} alt="TaskForge" className="ml-6 w-10 h-auto" />
-        <h1 className="text-gray-100 ml-12">TaskForge</h1>
-      </section>
-      <section className="pl-6 py-3 cursor-pointer hover:bg-gray-700">
+  const renderlists = (): JSX.Element[] => {
+    return [...Array(6).keys()].map(() => (
+      <section className="pl-6 py-2 cursor-pointer hover:bg-gray-700">
         <h2 className="text-sm mb-1">Todo List</h2>
         <h3 className="text-xs">8 Todo(s)</h3>
       </section>
-      <section className="pl-6 py-3 cursor-pointer  hover:bg-gray-700">
-        <h2 className="text-sm mb-1">Todo List</h2>
-        <h3 className="text-xs">8 Todos</h3>
-      </section>
-      <section className="pl-6 py-3 cursor-pointer hover:bg-gray-700">
-        <h2 className="text-sm mb-1">Todo List</h2>
-        <h3 className="text-xs">8 Todos</h3>
-      </section>
-      <section className="pl-6 py-3 cursor-pointer hover:bg-gray-700">
-        <h2 className="text-sm mb-1">Todo List</h2>
-        <h3 className="text-xs">8 Todos</h3>
-      </section>
-      <button className="mx-auto my-6 px-20 py-3 border-2 border-gray-100 rounded-full flex justify-center items-center hover:bg-gray-700">
+    ));
+  };
+  return (
+    <div className="fixed left-0 top-0 w-80 h-screen bg-gray-950 text-xl text-gray-100">
+      <div className="w-full flex items-center justify-start mt-3 mb-6">
+        <img src={logo} alt="TaskForge" className="ml-6 w-10 h-auto" />
+        <h1 className="text-gray-100 ml-12">TaskForge</h1>
+      </div>
+      <div className="w-full h-2/5 overflow-scroll">{renderlists()}</div>
+      <button className="mx-auto my-6 px-10 py-2 border-2 border-gray-100 rounded-md flex justify-center items-center hover:bg-gray-700">
         <IoAdd />
         Add List
       </button>
