e0db3d7cd869523a470db308e1f7592b77c5b905
evans-p <evanspoulaki@gmail.com>
2024-04-20 01:15:09 +0300

    "Wrap App component with BrowserRouter from react-router-dom"
    This diff adds the `BrowserRouter` component from `react-router-dom` and wraps the `App` component with it. This is likely done to

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./app/App.tsx";
 import "./i18n.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
