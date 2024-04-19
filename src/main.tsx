050035ec2a7130bccce1e0ddc1ec7344d4f6ca40
evans-p <evanspoulaki@gmail.com>
2024-04-20 01:37:54 +0300

    Add BrowserRouter to wrap App component
    The diff adds the BrowserRouter component from the "react-router-dom" package to wrap the App component. This enables client-side routing for the application.

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./app/App.tsx";
 import "./i18n.tsx";
@@ -6,6 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
