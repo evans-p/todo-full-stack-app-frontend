f3b65a59b33a15af033880e19b38b0173ae8acc9
evans-p <evanspoulaki@gmail.com>
2024-04-20 01:22:51 +0300

    Remove BrowserRouter from main.tsx
    The code diff shows the removal of BrowserRouter from the main.tsx file. The App component is now directly rendered within the StrictMode component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./app/App.tsx";
 import "./i18n.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
