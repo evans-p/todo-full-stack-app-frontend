50f7aaab09eb8654f99289f17dc92bc3cd2041e7
evans-p <evanspoulaki@gmail.com>
2024-04-20 14:41:06 +0300

    Add Axios and Google OAuth provider to project
    * Add Axios library as a dependency
    * Add Google OAuth provider to enable Google sign-in
    * Update main.tsx to include Google OAuth provider and wrap the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./app/App.tsx";
 import "./i18n.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
