9451d5908e265c71bb01c2b905a44aa964e63dae
evans-p <evanspoulaki@gmail.com>
2024-04-20 21:01:09 +0300

    Add state to toggle completed tasks menu and update DOM structure
    - Extracted rendering of header into its own component
    - Added state to toggle the visibility of completed tasks menu
    - Updated DOM structure to wrap completed tasks menu in a hidden

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { IoFilterOutline, IoStarOutline } from "react-icons/io5";
 import { IoIosArrowDropdown } from "react-icons/io";
@@ -7,7 +7,12 @@ import { RiDraggable } from "react-icons/ri";
 import Menu from "../components/Menu";
 
 const Main = () => {
+  const [completedMenuOpen, setCompletedMenuOpen] = useState<boolean>(false);
   const { t } = useTranslation();
+
+  const toogleCompletedMenu = () => {
+    setCompletedMenuOpen(!completedMenuOpen);
+  };
 
   const renderTodos = (): JSX.Element[] => {
     return [...Array(20).keys()].map((x) => (
@@ -35,7 +40,7 @@ const Main = () => {
     ));
   };
 
-  const renderCompletedTodos = () => {
+  const renderCompletedTodos = (): JSX.Element[] => {
     return [...Array(20).keys()].map((x) => (
       <div
         key={x + 30}
@@ -60,14 +65,12 @@ const Main = () => {
       </div>
     ));
   };
-  return (
-    <div className="flex">
-      <Menu />
-      <div className="ml-80 w-full h-full text-gray-800 dark:text-gray-100">
-        <div className="mx-6 mt-20 grid grid-cols-6 grid-rows-2">
-          <h2 className="text-xl font-bold col-span-full row-span-1 ml-3">
-            Todo List
-          </h2>
+
+  const renderHeader = (): JSX.Element => {
+    return (
+      <div className="mx-6 h-36 flex flex-col items-start justify-end">
+        <h2 className="ml-3 text-xl font-bold">Todo List</h2>
+        <section className="w-full grid grid-cols-6 grid-rows-1">
           <h3 className="col-span-3 row-span-1 my-auto ml-3">
             {t("main.list.todo")}
           </h3>
@@ -85,20 +88,39 @@ const Main = () => {
               <IoFilterOutline />
             </span>
           </section>
+        </section>
+      </div>
+    );
+  };
+
+  return (
+    <div className="flex">
+      <Menu />
+      <div className="ml-80 w-screen h-screen text-gray-800 dark:text-gray-100">
+        {renderHeader()}
+        <div className="h-[calc(50%-4.5rem)] overflow-scroll no-scrollbar">
+          {renderTodos()}
         </div>
-        <div className="h-40 overflow-scroll">{renderTodos()}</div>
-        <div className="mx-6 my-6 rounded-sm  dark:bg-gray-700 bg-gray-300">
-          <button className="w-full group">
-            <section className="flex items-center justify-between">
-              <h3 className="pl-3">{t("main.list.completed")}</h3>
-              <span className="text-2xl mr-2 p-2 cursor-pointer rounded-full hover:bg-white hover:shadow dark:hover:bg-gray-600 dark:hover:shadow-gray-400 dark:hover:shadow-sm">
-                <IoIosArrowDropdown />
-              </span>
-            </section>
-            <section className="w-full h-48 overflow-scroll no-scrollbar hidden group-focus:grid">
-              {renderCompletedTodos()}
-            </section>
+        <div className="mx-6 py-3 h-[calc(50%-4.5rem)]">
+          <button
+            className="w-full dark:bg-gray-700 bg-gray-300 flex justify-between items-center"
+            onClick={toogleCompletedMenu}
+            onBlur={() => setCompletedMenuOpen(false)}
+          >
+            <h3 className="pl-3">{t("main.list.completed")}</h3>
+            <span className="text-2xl mr-3 p-1 cursor-pointer rounded-full hover:bg-white dark:hover:bg-gray-600">
+              <IoIosArrowDropdown />
+            </span>
           </button>
+          <section
+            className={
+              completedMenuOpen
+                ? "w-full h-[calc(100%-2rem)] overflow-scroll no-scrollbar dark:bg-gray-700 bg-gray-300 rounded-b-sm"
+                : "w-full h-[calc(100%-2rem)] overflow-scroll no-scrollbar dark:bg-gray-700 bg-gray-300 rounded-b-sm hidden"
+            }
+          >
+            {renderCompletedTodos()}
+          </section>
         </div>
       </div>
     </div>
