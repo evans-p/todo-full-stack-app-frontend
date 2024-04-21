9b879dc49c00d3785ad4dce51e2dd37a7c84c9df
evans-p <evanspoulaki@gmail.com>
2024-04-21 19:46:17 +0300

    "Add jwt-decode package, update Login page to use Google Login with @react-oauth/google and jwt-decode for token decoding"

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
-import { FcGoogle } from "react-icons/fc";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 
 import logo from "../assets/images/logo.png";
+import { redirect } from "react-router-dom";
 
 const Login = () => {
   const { t } = useTranslation();
+
   return (
     <div className="h-screen w-screen bg-gradient-image bg-contain bg-no-repeat bg-bottom flex justify-center items-center">
       <section className="w-96 h-96 bg-white bg-opacity-40 backdrop-blur-xl rounded ring-2 ring-black/5">
@@ -13,15 +15,22 @@ const Login = () => {
         <h2 className="text-3xl text-gray-800  tracking-wider text-center dark:text-gray-100">
           {t("login.title")}
         </h2>
-        <h1 className="text-5xl text-gray-800 font-bold tracking-wide text-center dark:text-gray-100">
+        <h1 className="text-5xl text-gray-800  font-bold tracking-wide text-center dark:text-gray-100">
           TaskForge
         </h1>
-        <button className="w-80 mx-auto mt-16 rounded-full bg-white hover:bg-gray-100 py-2 px-4 shadow hover:shadow-md flex items-center justify-center">
-          <FcGoogle className="text-2xl mr-3" />
-          <span className="text-xl text-gray-800 font-semibold">
-            {t("login.googleButton")}
-          </span>
-        </button>
+        <div className="flex justify-center items-center mt-6">
+          <GoogleLogin
+            onSuccess={(response: CredentialResponse) => {
+              console.log(response.clientId);
+            }}
+            onError={() => {
+              redirect("/");
+            }}
+            width={250}
+            shape="square"
+            size="large"
+          />
+        </div>
       </section>
     </div>
   );
